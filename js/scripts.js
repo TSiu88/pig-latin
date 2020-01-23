53b3992128cdff01f2c162bf50027e145e5efd1a
jason mcgrady and Tiffany Siu <jason@>
2020-01-23 11:13:25 -0800

    add exception for y

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,16 @@ function vowelCheck(letter1){
 
 //Check how many consonants in a row and change to pig latin
 function consTranslate(text){
+  var tempString = text.toLowerCase();
+  var consecutiveIndex = 1;
+  var letterArray = tempString.split("");
+
   if(text.length === 1){
     return text + "ay";
+  }else if(tempString.charAt(0) === "y"){
+    letterArray.shift();
+    return letterArray.join("") + text.charAt(0) + "ay";
   }else{
-    var tempString = text.toLowerCase();
-    var letterArray = tempString.split("");
-    var consecutiveIndex = 1;
     var foundVowel = false;
     for(var i=1; i<letterArray.length && !foundVowel; i++){
       if(!vowelCheck(letterArray[i])){
@@ -39,7 +43,7 @@ function consTranslate(text){
         }
       }
     }
-    console.log(consecutiveIndex);
+    
     var startLetters = text.slice(0, consecutiveIndex);
     var endLetters = text.slice(consecutiveIndex);
     return endLetters + startLetters + "ay";
